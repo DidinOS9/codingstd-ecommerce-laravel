27f4b1ee08ceb36383495356a7f5b2d84564458c
DidinOS9 <didinoktaviandasetiawan@gmail.com>
2023-02-13 14:43:03 +0700

    custom regist form

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,7 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Product;
-use \Faker\Generator as Faker;
+use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
