38e0f4ad246e9abe798307af4c1c3d9581afabfa
DidinOS9 <didinoktaviandasetiawan@gmail.com>
2023-02-15 00:00:40 +0700

    menampilkan data user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,51 +20,13 @@
       <div class="content">
         <!-- Data User -->
       <label for="">Name</label>
-      <p></p>
+      <p>{{ Auth::user()->name }}</p>
       <label for="">Email</label>
-      <p></p>
+      <p>{{ Auth::user()->email }}</p>
       <label for="">Address</label>
-      <p></p>
-      <label for="">Date of Birth</label>
-      <p></p>
-      <label for="">Gender</label>
-      <p></p>
-      <label for="">Account Balance</label>
-      <!-- TopUp -->
-      {{-- @if($topup == null)
-      <p style="color:red">Havent top up yet</p>
-      @else
-      <p style="color:green">Rp{{number_format($topup->amount)}}</p>
-      @endif --}}
-      <button class="btn btn-topup" data-toggle="modal" data-target="#exampleModal">Top-Up Now</button>
-          </div>
-      </div>
+      <p>{{ Auth::user()->address }}</p>
+      <label for="">Phone</label>
+      <p>{{ Auth::user()->phone }}</p>
 </div>
-</div>
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <!-- Topup -->
-  <form action="" method="POST">
-      @csrf
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Top Up</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <div class="form-group mt-2">
-          <label for="topup">Top Up Amount</label>
-          <input type="number" class="form-control" id="topup" name="amount">
-      </div>
-      </div>
-      <div class="modal-footer">
-          <button type="submit" class="btn btn-primary">Top Up</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </form>
-  </div>
 </div>
 @endsection
