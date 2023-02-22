6869f94dd292540bf32364a0bcccc66a8b1ca17f
DidinOS9 <didinoktaviandasetiawan@gmail.com>
2023-02-22 12:13:28 +0700

    menampilkan detail product

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/shop', 'ShopController@index');
-Route::get('/shop/detail', 'ShopController@show');
+Route::get('/shop/detail/{id}', 'ShopController@show');
 Route::get('/cart', 'ShopController@index');
